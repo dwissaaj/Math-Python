48585c9347e5095984286116db534754a9cfaabe
ParkhaeShin <71976179+ParkhaeShin@users.noreply.github.com>
2020-10-25 16:52:36 +0700

    Add files via upload

---
+++ Math.py
@@ -0,0 +1,35 @@
+# function for squared
+def squared(a):
+    return a * a
+
+
+def division(a, b):
+    return int(a / b)
+
+
+def multiplied(a, b):
+    return a * b
+
+
+def addition(a, b):
+    return a + b
+
+
+def subtraction(a, b):
+    return a - b
+
+
+def circumferenceforsquare(a):
+    return 4 * a
+
+
+def circumferenceforectangle(a, b):
+    return 2 * (a + b)
+
+
+def areasforsquare(a):
+    return print(a * a, " kuadrat")
+
+
+def areasforrectangle(a, b):
+    return print(a * b, " kuadrat")
